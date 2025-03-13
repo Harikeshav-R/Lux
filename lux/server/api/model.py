9e4390a8608c5a11616c671c9588701629866697
Harikeshav R <r.harikeshav@gmail.com>
2025-03-13 13:07:48 -0400

    Raise 404 if provider name does not exist

--- lux/server/api/model.py
+++ lux/server/api/model.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from g4f import ProviderType
+from g4f.Provider import ProviderUtils
 from sqlalchemy.orm import Session
 
 from lux.server.core.db import get_db
@@ -17,5 +19,9 @@ router = APIRouter()
 def read_all_models_endpoint(*, db: Session = Depends(get_db), provider_name: str | None = None):
     if not provider_name:
         return read_all_models(db)
+
+    # See different implementation in G4F API code
+    if provider_name not in ProviderUtils.convert:
+        raise HTTPException(status_code=404, detail="The given provider does not exist.")
 
     return read_model_by_provider(db, provider_name)
