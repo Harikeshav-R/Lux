b1827843d62dcaf0d66614fda40f29a05ca5fc63
Harikeshav R <r.harikeshav@gmail.com>
2025-03-12 22:05:09 -0400

    Create LLM model

---
+++ lux/server/models/model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lux.server.core.db import Base
+
+
+class Model(Base):
+    __tablename__ = 'model'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+
+    model_name: Mapped[str] = mapped_column(String, nullable=False)
+    base_provider: Mapped[str] = mapped_column(String, nullable=False)
+    is_provider: Mapped[bool] = mapped_column(Boolean, nullable=False)
