2f50af856d81d1c8c1b26ad42fca81fed56b5d1c
Harikeshav R <r.harikeshav@gmail.com>
2025-03-12 21:56:48 -0400

    Create LLM schema

---
+++ lux/server/schemas/model.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class ModelBase(BaseModel):
+    model_name: str
+    base_provider: str
+    is_provider: bool
+
+
+class ModelInDBBase(ModelBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Model(ModelInDBBase):
+    pass
