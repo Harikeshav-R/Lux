bb52a773f8cdc43e55e7c415f2493e95cab25d47
Harikeshav R <r.harikeshav@gmail.com>
2025-03-14 22:30:29 -0400

    Create Model type to store model information

--- lux/llm/client.py
+++ lux/llm/client.py
@@ -1,7 +1,44 @@
+from typing import Type
+
 from g4f import Client as G4FClient
+from g4f import models
+from g4f.Provider import RetryProvider
+from g4f.providers.types import BaseProvider
 
 from lux.llm.conversation import Conversation
 from lux.utils.settings import settings
+
+
+class Model:
+    all_models = \
+        {
+            str(model.name):
+                {
+                    "image": isinstance(model, models.ImageModel),
+                    "vision": isinstance(model, models.VisionModel),
+                    "providers":
+                        [
+                            (getattr(provider, "parent", provider.__name__), provider)
+                            for provider in providers
+                            if provider.working
+                               and not provider.needs_auth
+                               and not getattr(provider, "use_nodriver", False)
+                        ]
+                }
+            for model, providers in models.__models__.values()
+        }
+
+    def __init__(self, model_name: str):
+        if model_name not in self.all_models:
+            raise ValueError(
+                f"'{model_name}' is not a valid model! Available models are: {', '.join(self.all_models.keys()).strip(', ')}")
+
+        self.model_name: str = model_name
+        self.image_model: bool = self.all_models[model_name]["image"]
+        self.vision_model: bool = self.all_models[model_name]["vision"]
+        self.providers: dict[str, Type[BaseProvider]] = {
+            provider_name: provider_class for provider_name, provider_class in self.all_models[model_name]["providers"]
+        }
 
 
 class Client:
